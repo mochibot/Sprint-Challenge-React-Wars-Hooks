c93fad2b4895420dd2663fcd7e10cb535e3deb9d
mochibot <amusaur@gmail.com>
2019-07-12 14:31:02 -0700

    changed previous and forward buttons to semantic-ui pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Header, Button } from 'semantic-ui-react';
+import { Header, Button, Pagination } from 'semantic-ui-react';
 
 import Loading from './components/Loading'
 import CharacterList from './components/CharacterList';
@@ -12,25 +12,26 @@ import './App.css';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [data, setData] = useState([]);
+  const [data, setData] = useState('');
+  const [charList, setCharList] = useState([]);
   const [page, setPage] = useState(1);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
   
   useEffect(() => {
     axios.get(`https://swapi.co/api/people/?page=${page}`)
       .then(response => {
         console.log('fetch data success: ', response)
         setData(response.data);
+        setCharList(response.data.results);
       })
       .catch(error => {
         console.log('error fetching data: ', error);
       })
   }, [page])
   
-
+   /*
   const nextPage= () => {
     if (data.next === null) {
       alert('At the start of data');
@@ -47,14 +48,23 @@ const App = () => {
     }
   }
 
+  */
+  const maxPage = Math.ceil(data.count / 10);
+
+  const pageChangeHandler = (e, data) => {
+    console.log(data)
+    setPage(data.activePage)
+  } 
+
   return (
     <div className="App">
-      <Header style={{fontSize: '30px', paddingTop: '10px'}}>React Wars</Header>
-      <Button.Group style={{padding: '15px 0'}}>
+      <Header className='AppHeader' style={{padding: '20px', fontSize: '30px'}}>React Wars</Header>
+      {data && <Pagination style={{margin: '20px 0'}} activePage={page} totalPages={maxPage} ellipsisItem={null} siblingRange={2} onPageChange={pageChangeHandler}/>}
+      {/* <Button.Group style={{padding: '15px 0'}}>
         <Button onClick={prevPage} labelPosition='left' icon='left chevron' content='Previous'></Button>
         <Button onClick={nextPage} labelPosition='right' icon='right chevron' content='Forward'></Button>
-      </Button.Group>
-      {data.length === 0 ? <Loading/> : <CharacterList characters={data.results} />}
+      </Button.Group> */}
+      {!data ? <Loading/> : <CharacterList characters={charList} />}
     </div>
   );
 }
--- starwars/src/components/Loading.js
+++ starwars/src/components/Loading.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Dimmer, Loader, Image, Segment  } from 'semantic-ui-react';
+import { Dimmer, Loader, Segment } from 'semantic-ui-react';
 
 const Loading = () => {
   return (
-    <Segment style={{padding: '40px'}}>
+    <Segment style={{padding: '40px', width: '50%', margin: '0 auto'}}>
       <Dimmer active inverted>
         <Loader inverted>Loading</Loader>
       </Dimmer>
-      <Image src='/images/wireframe/short-paragraph.png' />
     </Segment>
   )
 }
